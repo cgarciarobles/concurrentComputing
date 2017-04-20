7567afcd2b9866471c0f278b3260038e163d3132
Carlos Garcia <carlosfipq@gmail.com>
2017-04-20 15:26:08 -0600

    primer ejercicio terminado

--- hone.py
+++ hone.py
@@ -2,33 +2,52 @@ import random
 
 
 arreglo = ['a','a','a','a','a']
+contadorA = 0
+contadorO = 0
 
 def procA(contador):
+    global contadorA
     arreglo[contador] = 'a'
     contador += 1
+    contadorA += 1
     return contador
 
+
 def procB(contador):
+    global contadorA
+    global contadorO
     if contador == 0:
         print "El limite de bs esta definido por la cantidad de as"
     else:
         if arreglo[contador-1] == 'b':
             print "No pueden escribirse dos b seguidas"
         else:
-            arreglo[contador] = 'b'
-            contador += 1
+            if contadorA > contadorO:
+                arreglo[contador] = 'b'
+                contador += 1
+                contadorO += 1
+            else:
+                print "El limite esta definido por la cantidad de as"
     return contador
 
+
 def procC(contador):
+    global contadorA
+    global contadorO
     if contador == 0:
         print "El limite de cs esta definido por la cantidad de as"
     else:
         if arreglo[contador-1] == 'c':
             print "No pueden escribirse dos c seguidas"
         else:
-            arreglo[contador] = 'c'
-            contador += 1
+            if contadorA > contadorO:
+                arreglo[contador] = 'c'
+                contador += 1
+                contadorO += 1
+            else:
+                print "El limite esta definido por la cantidad de as"
     return contador
+
 
 contador = 0
 while contador < 5:
